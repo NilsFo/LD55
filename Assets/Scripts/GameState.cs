11bbd34b18b1d8c99a69e041e808da5290d0e722
PatrickQ <Patrick@Quiskamp.de>
2024-04-14 19:56:00 +0200

    Quick Fix

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.Events;
 using Random = System.Random;
